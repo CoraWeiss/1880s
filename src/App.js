ffb215ddb7917e942c409e418dd0143a92cac1db
Christmas Freud <cora.weiss@gmail.com>
2025-01-26 18:17:05 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import ArtworkCollection from './components/ArtworkCollection';
+
+function App() {
+  return (
+    <div>
+      <ArtworkCollection />
+    </div>
+  );
+}
+
+export default App;
